3c1d6509bdb2d0e932b7aaf11d2790824d75349c
santiago garcia <105396418+san270131@users.noreply.github.com>
2022-08-07 20:47:36 +0000

    subindo verificação de formulario para gitHub pages

--- formulario.js
+++ formulario.js
@@ -1,10 +1,74 @@
-var inputs = document.getElementsByClassName('formulario__input');
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keyup', function(){
-        if(this.value.length>=1){
-            this.nextElementSibling.classList.add('fixar');
-        }else {
-            this.nextElementSibling.classList.remove('fixar')
-        }
-    });
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const telefone = document.getElementById("telefone");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const telefoneValue = telefone.value;
+
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome é obrigatório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (telefoneValue === "") {
+    setErrorFor(telefone, "O telefone é obrigatório.");
+  } else if (telefoneValue.length < 13) {
+    setErrorFor(telefone, "O telefone precisa ter 13 caracteres incluindo parenteses.");
+  } else {
+    setSuccessFor(telefone);
+  }
+
+  const formControls = form.querySelectorAll(".form-control");
+
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form-control success";
+  });
+
+  if (formIsValid) {
+    console.log("O formulário está 100% válido!");
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adiciona a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }

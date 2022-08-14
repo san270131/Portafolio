bf09aae206fc033eea77ef3dfc16c3984a4893c3
san270331 <jb345815@gmail.com>
2022-08-14 19:38:44 -0300

    criando arquivos eslintrc.json e pasta componentes

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,19 +1,27 @@
-import React, { useEffect } from "react";
-import Tmdb from './Tmdb';
+import React, { useEffect, useState } from 'react';
+import Tmdb from '../src/Tmdb';
+import movieRow from './componentes/movieRow';
 
 export default () => {
-  useEffect(() =>{
-    const loadAll = async () =>{
-      let list = await Tmdb.getHomeList();
-      console.log(list);
-    }
-    loadAll();
-  }, []);
 
-  return (
-    <div>
-    Olá Mundo!
-    </div>
-  );
+    const [movieList, setMovieList] = useState([]);
+
+    useEffect(()=>{
+        const loadAll = async () => {
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+        }
+
+        loadAll();
+    }, []);
+
+    return (
+        <div className="page">
+            <section className="list">
+                {movieList.map((item, key)=>(
+                    <movieRow />
+                ))}
+            </section>
+        </div>
+    );
 }
-
---
+++ netflixclone/src/componentes/movieRow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../componentes/movieRow.css';
+
+export default () => {
+    return (
+        <div>
+            Lista do filme
+        </div>
+    )
+}

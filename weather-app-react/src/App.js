51ae3393551a74a4b0b62757766fe7477bf22f44
san270331 <jb345815@gmail.com>
2022-08-14 23:46:59 -0300

    criando o html no arquivo app.js

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+//const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=7a6e2ede11750c496fa5fb7350b3e713`
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className='app'>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>60°F</h1>
+          </div>
+          <div className='descrption'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            12 MP
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
